e217b4813c417572eac7e8466c7e9c562fc46c07
Tom Ziegmann <tom_ziegmann@mcafee.com>
2018-04-11 11:39:10 -0700

    Add ENSWC HF EA

---
+++ mcafee-ens-webcontrol-hotfix-version-ea.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## mcafee-ens-webcontrol-hotfix-version-ea.sh
+## version 1.0
+## 
+## Author: Tom Ziegmann, McAfee Professional Services
+## E-Mail: Tom_Ziegmann@McAfee.com
+
+# This script has been verified to work on McAfee Endpoint Security 10 for Mac
+
+FMHotfix=`cat /usr/local/McAfee/fmp/config/WebProtection/FMConfig.xml | egrep "<Hotfixes>.*</Hotfixes>" |sed -e "s/<Hotfixes>\(.*\)<\/Hotfixes>/\1/"|tr -d " "|tr -d "\t"|tr -d "\n"|tr -d "\r"`
+
+echo '<result>'$FMHotfix'</result>'
